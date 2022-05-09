9b82164dd726d54e05486c8f9a2ba1fc49d3c614
Klingert, Luca <luca.klingert@student.fhws.de>
2022-05-09 11:53:49 +0200

    More mongo

--- backend/src/main/java/app/MongoloidTest.java
+++ backend/src/main/java/app/MongoloidTest.java
@@ -1,11 +1,15 @@
 package main.java.app;
 
 import com.mongodb.MongoException;
+import static com.mongodb.client.model.Filters.eq;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.Sorts;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
 public class MongoloidTest {
 
@@ -14,11 +18,25 @@ public class MongoloidTest {
 
     MongoDatabase db = client.getDatabase("TestDB");
 
-    MongoCollection col = db.getCollection("TestProjects");
+    MongoCollection<Document> col = db.getCollection("TestProjects");
 
-    Document doc = new Document("_id", "0").append("name", "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
-    col.insertOne(doc);
+        Bson projectionFields = Projections.fields();
 
+        Document doc = col.find(eq("_id", "0"))
+                .projection(projectionFields)
+                .first();
+
+        if (doc == null)
+        {
+            System.out.println("No results found.");
+        }
+        else
+        {
+            System.out.println(doc.toJson());
+        }
+
+   // Document doc = new Document("_id", "6").append("name", "Oh the misery");
+    // col.insertOne(doc);
     client.close();
 
     }
