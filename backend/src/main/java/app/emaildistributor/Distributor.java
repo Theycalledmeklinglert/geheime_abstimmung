7259d2d06f30dc645577b5de8c871fd77469ca50
Braunger, Tim <tim.braunger@student.fhws.de>
2022-05-14 15:04:57 +0200

    Basic Email Distributor

---
+++ backend/src/main/java/app/emaildistributor/Distributor.java
@@ -0,0 +1,52 @@
+package main.java.app.emaildistributor;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.*;
+
+public class Distributor {
+    public static void main(String[] args) {
+
+        String recipient = "blablacaodg@gmail.com";
+        String sender = "secret.vote.project@gmail.com";
+        String host = "smtp.gmail.com";
+
+        Properties properties = System.getProperties();
+
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.socketFactory.port", "465");
+        properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(sender, "Project420");
+            }
+        });
+        session.setDebug(true);
+
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(sender));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse(recipient)
+            );
+            message.setSubject("This is the Subject Line!");
+            message.setText("Link for the vote");
+            Transport.send(message);
+            System.out.println("Done");
+        }
+        catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
