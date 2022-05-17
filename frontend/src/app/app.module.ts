8b4b7361f44dae0281889f883176d3d91167d981
SchmittMichael <michael.schmitt.9@student.fhws.de>
2022-05-17 21:10:36 +0200

    Updated surveyUI (no javascript)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field'
 import {MatButtonModule} from '@angular/material/button'
+import {MatRadioModule} from '@angular/material/radio';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 import {QuestionComponent} from "../lib/question/question.component";
-import {RouterModule, Routes} from "@angular/router";
 import {MainmenuComponent} from "../lib/router/mainmenu/mainmenu.component";
 import {LoginComponent} from "../lib/router/login/login.component";
 import { SurveyComponent } from '../lib/router/survey/survey.component';
@@ -44,6 +45,8 @@ import { AppRoutingModule } from './app-routing.module';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    MatRadioModule,
+    MatCheckboxModule,
     BrowserAnimationsModule,
     AppRoutingModule
   ],
