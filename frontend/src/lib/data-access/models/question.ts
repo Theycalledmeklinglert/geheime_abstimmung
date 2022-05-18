2e865b5bf1a36e75d1e7f07a4701a6e0846be935
Wanka, Marco <marco.wanka@student.fhws.de>
2022-05-18 12:05:05 +0200

    fix bug resulting of merge

--- frontend/src/lib/data-access/models/question.ts
+++ frontend/src/lib/data-access/models/question.ts
@@ -3,13 +3,13 @@ export interface Question{
   id: number;
   title: string;
   description?: string;
-  type: string;
+  type?: string;
 
   yesNo?: boolean;
   fixedAnswers?: string[];
   multipleChoiceAnswer?: boolean[]
   individualAnswer?: string;
-  
+
   visible?:boolean;
 }
 
