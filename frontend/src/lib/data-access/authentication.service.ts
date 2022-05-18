082e8cc33061146d09b78a564b07e10dbd2f28dc
SchmittMichael <michael.schmitt.9@student.fhws.de>
2022-05-18 13:47:48 +0200

    updated authentication and backend service

--- frontend/src/lib/data-access/authentication.service.ts
+++ frontend/src/lib/data-access/authentication.service.ts
@@ -1,27 +1,36 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-import {map} from "rxjs/operators"
-
-export interface LoginForm {
-  email: string;
-  password: string;
-}
+import {EncryptionService} from "./encryption.service";
+import {BackendService} from "./backend.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor(private httpClient: HttpClient) { }
 
-  login(loginForm: LoginForm) {
-
-    return this.httpClient.post<any>('/api/users/login', {email: loginForm.email, password: loginForm.password}).pipe( //temporäre uri
-      map((token) => {
-        console.log('token');
-        localStorage.setItem('blog-token', token.access_token);
-        return token;
-      })
-    )
+  constructor(private cryptService: EncryptionService, private backendS: BackendService) {
   }
+
+
+  getSessionid(username: string, password: string): void {
+    //-> call encryption service to generate public key
+    let myPublicKey: string = this.cryptService.generateKeys().toString();
+    //-> call backend with postRequest(public Key of frontend) to get response (public key of Backend)
+    let backendPublicKey: string = "test"; // (casttostring) this.backendS.setFrontendPublicKey(myPublicKey);
+    //-> store  public Key of Backend in local storage
+    //-> here:
+    localStorage.setItem("backendpublickey",backendPublicKey);
+
+
+    //let user: string = this.cryptService.encryptMessage(backendPublicKey,jsonofusernameandpasssord,);
+
+    //-> call backend with get(encrypted username and userpassword)[queryparameter] to get sessionid
+    //this.backendS.getSessionKey
+    //-> store sessionid in local storage
+    //-> here:
+    localStorage.setItem("sessionid","SERVERRESPONSE");
+  }
+
+
+
 }
--- frontend/src/lib/data-access/backend.service.ts
+++ frontend/src/lib/data-access/backend.service.ts
@@ -6,4 +6,25 @@ import { Injectable } from '@angular/core';
 export class BackendService {
 
   constructor() { }
+  getVotecontainerofsurveyLeader(): void {
+  }
+
+  getUsernameofsurveyLeader():string{
+    return "No Connection";
+  }
+
+  setpasswordofSurveyLeader(): void{
+  }
+
+  setSurveyLeader(): void{}
+
+  deleteSurveyLeader(): void{
+  }
+
+  setnewVote(): void{
+  }
+
+
+
+
 }
