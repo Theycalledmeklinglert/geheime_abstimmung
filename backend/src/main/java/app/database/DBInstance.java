07b8733931ca7006dbc7b28e1c9368f8482f5d77
Klingert, Luca <luca.klingert@student.fhws.de>
2022-06-01 17:28:39 +0200

    Bufgfix postAnswer()

--- backend/src/main/java/app/database/DBInstance.java
+++ backend/src/main/java/app/database/DBInstance.java
@@ -319,12 +319,12 @@ public class DBInstance {
         Document poll = optPoll.get();
         ArrayList<String> tokens = (ArrayList<String>) poll.get("tokens");
 
-        if (tokens.contains(answer.getString("token"))) {
+        if (tokens.contains(token)) {
             Bson filter = Filters.eq("_id",new ObjectId(pollID));
             Bson delete = Updates.pull("tokens", token);
             pollCol.updateOne(filter, delete);
 
-            filter = Filters.eq("poll_id", token);
+            filter = Filters.eq("token", token);
             BasicDBObject listItem = new BasicDBObject("answers", answer);
             Bson pushToQuestList = new BasicDBObject("$push", listItem);
 
