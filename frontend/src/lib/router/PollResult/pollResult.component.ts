cf305899e1d19278694df21a2085dd7e20180a56
Gehring, Johannes (BIN 4) <johannes.gehring.1@student.fhws.de>
2022-06-02 15:16:03 +0200

    donut chart test

--- frontend/src/lib/router/PollResult/pollResult.component.ts
+++ frontend/src/lib/router/PollResult/pollResult.component.ts
@@ -1,9 +1,17 @@
-import { Component} from "@angular/core";
+import {Component, ViewChild} from "@angular/core";
 import {LoginComponent} from "../login/login.component";
 import {BackendService} from "../../data-access/service/backend.service";
 import {AuthenticationService} from "../../data-access/service/authentication.service";
 import {MatPaginatorModule} from '@angular/material/paginator';
+import {ChartComponent, ApexNonAxisChartSeries, ApexResponsive, ApexChart, ApexOptions} from "ng-apexcharts";
 
+export type ChartOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  responsive: ApexResponsive[];
+  labels: any;
+  colors: ApexOptions[];
+};
 
 @Component({
   selector: 'surveyResult',
@@ -12,13 +20,32 @@ import {MatPaginatorModule} from '@angular/material/paginator';
 })
 
 export class PollResultComponent {
-
-
-
+  @ViewChild("chart") chart: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
 
 
   constructor() {
-  }
+    this.chartOptions = {
+      series: [50, 50],
+      chart: {
+        type: "donut"
+      },
+      labels: ["Yes", "No"],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: "bottom"
+            }
+          }
+        }
+      ]
+    };
+}
 
 
   getIDofclickedPoll():string{
