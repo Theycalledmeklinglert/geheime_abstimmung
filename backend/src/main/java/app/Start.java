7c1c6de62313dccbd9de56700f735498bcf1108b
Gehring, Johannes (BIN 4) <johannes.gehring.1@student.fhws.de>
2022-07-19 21:24:09 +0200

    some fixes

--- backend/src/main/java/app/Start.java
+++ backend/src/main/java/app/Start.java
@@ -9,40 +9,38 @@ import org.apache.catalina.webresources.StandardRoot;
 
 import java.io.File;
 
-public class Start
-{
-	private static final String CONTEXT_PATH = "";
-	private static final String WEB_APP_LOCATION = "backend/src/main/webapp/";
-	private static final String WEB_APP_MOUNT = "/WEB-INF/classes";
-	private static final String WEB_APP_CLASSES = "target/classes";
+public class Start {
+    private static final String CONTEXT_PATH = "";
+    private static final String WEB_APP_LOCATION = "backend/src/main/webapp/";
+    private static final String WEB_APP_MOUNT = "/WEB-INF/classes";
+    private static final String WEB_APP_CLASSES = "target/classes";
 
-	public static void main( final String[] args ) throws Exception
-	{
+    public static void main(final String[] args) throws Exception {
 
-		final Tomcat tomcat = new Tomcat( );
-		tomcat.setPort(8080);
+        final Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+        Connector httpsConnector = new Connector();
+        httpsConnector.setPort(8080);
+        httpsConnector.setSecure(true);
+        httpsConnector.setScheme("https");
+        httpsConnector.setAttribute("SSLCertificateFile", "../frontend/src/ssl/certificate.pem");
+        httpsConnector.setAttribute("SSLCertificateKeyFile", "../frontend/src/ssl/key.pem");
+        httpsConnector.setAttribute("clientAuth", "false");
+        httpsConnector.setAttribute("sslProtocol", "TLS");
+        httpsConnector.setAttribute("SSLEnabled", true);
+        httpsConnector.setURIEncoding("UTF-8");
+        tomcat.setConnector(httpsConnector);
 
-		final Context context = tomcat.addWebapp( CONTEXT_PATH, new File( WEB_APP_LOCATION ).getAbsolutePath( ) );
-		final String pathToClasses = new File( WEB_APP_CLASSES ).getAbsolutePath( );
-		final WebResourceRoot resources = new StandardRoot( context );
-		final DirResourceSet dirResourceSet = new DirResourceSet( resources, WEB_APP_MOUNT, pathToClasses, "/" );
+        final Context context = tomcat.addWebapp(CONTEXT_PATH, new File(WEB_APP_LOCATION).getAbsolutePath());
+        final String pathToClasses = new File(WEB_APP_CLASSES).getAbsolutePath();
+        final WebResourceRoot resources = new StandardRoot(context);
+        final DirResourceSet dirResourceSet = new DirResourceSet(resources, WEB_APP_MOUNT, pathToClasses, "/");
 
-		resources.addPreResources( dirResourceSet );
-		context.setResources( resources );
+        resources.addPreResources(dirResourceSet);
+        context.setResources(resources);
 
-		tomcat.start( );
-		tomcat.getServer( ).await( );
-	}
+        tomcat.start();
+        tomcat.getServer().await();
+    }
 }
 
-//	Connector httpsConnector = new Connector();
-//		httpsConnector.setPort(8080);
-//				httpsConnector.setSecure(true);
-//				httpsConnector.setScheme("https");
-//				httpsConnector.setAttribute("SSLCertificateFile", "../frontend/src/ssl/certificate.pem");
-//				httpsConnector.setAttribute("SSLCertificateKeyFile", "../frontend/src/ssl/key.pem");
-//				httpsConnector.setAttribute("clientAuth", "false");
-//				httpsConnector.setAttribute("sslProtocol", "TLS");
-//				httpsConnector.setAttribute("SSLEnabled", true);
-//				httpsConnector.setURIEncoding("UTF-8");
-//tomcat.setConnector(httpsConnector);
